490744d1d1351bc62aca70f71ab8fa879a7e265d
ghada <ghadabensalem0@gmail.com>
2019-04-26 17:19:36 +0100

    login componant

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,Validators } from '@angular/forms';
+import * as jwt_decode from "jwt-decode";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm:FormGroup;
+  message = '';
 
-  constructor() { }
+  constructor(private apiService : ApiService,private router: Router) {
+
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(3)])
+    })
+  }
 
   ngOnInit() {
   }
+  loginBtn(){
+    this.message = ''
+    if(this.loginForm.valid){
+      this.apiService.login(this.loginForm.value).subscribe((res:any) =>{
+        console.log(res);
+        if(res.message === 'ok'){
+          // redirection
+         // console.log( jwt_decode(res.token))
+          localStorage.setItem('token', res.token);
+          this.router.navigate(['/to-do-list']);
 
+        } else{
+          this.message = res.message;
+        }
+      })
+    }
+  }
 }
