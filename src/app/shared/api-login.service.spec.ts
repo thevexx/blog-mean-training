5f7232a5e52d5d4eaf16b4cf340813a6ca083b01
ghada <ghadabensalem0@gmail.com>
2019-04-30 14:18:08 +0100

    change

--- src/app/shared/api-login.service.spec.ts
+++ src/app/shared/api-login.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ApiLoginService } from './api-auth.service';
+import { ApiAuthService } from '../shared/api-auth.service';
 
 describe('ApiLoginService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: ApiLoginService = TestBed.get(ApiLoginService);
+    const service: ApiAuthService = TestBed.get(ApiAuthService);
     expect(service).toBeTruthy();
   });
 });
