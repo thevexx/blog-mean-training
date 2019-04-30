6a20a240024c6c318f540e2ce51e81473b248a27
hagerCh <hagerchaabane1@gmail.com>
2019-04-30 20:06:46 +0200

    idkk

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const comment = new mongoose.Schema({
   comment: String,
-  date: { type: String, default: Date.now() }
+  date:{type:String, default: Date.now()}
 })
 
 module.exports = mongoose.model('comment', comment);
