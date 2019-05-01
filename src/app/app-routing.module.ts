f81ec956ce08269bc2a9cd9750979577d1f36a23
yosraBouasker <yosrabouasker5@gmail.com>
2019-05-01 18:08:28 +0100

    account component html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ArticleComponent } from './article/article.component';
+import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'article', component: ArticleComponent}
+  {path: 'article', component: ArticleComponent},
+  {path: 'account', component: AccountComponent}
 ];
 
 @NgModule({
